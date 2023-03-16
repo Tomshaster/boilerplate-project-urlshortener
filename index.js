73ebacf4525894bdb0f07280276c2987f0a533f6
Glitch (crystalline-pear-slicer) <none>
2023-03-16 00:15:45 +0000

    8️⃣🥊 Updated with Glitch

--- index.js
+++ index.js
@@ -2,9 +2,17 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+const urlParser = bodyParser.urlencoded({ extended: true })
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
+
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 app.use(cors());
 
@@ -18,6 +26,15 @@ app.get('/', function(req, res) {
 app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
+
+app.post('/api/shorturl', urlParser, function(req, res) {
+  console.log(req.body)
+  res.json({body: req.body})
+})
+
+app.get('/api/shorturl/:linkId', function(req, res){
+  
+})
 
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
